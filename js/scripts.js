af4ce0fea3d17de07723c411ebd5653396dc7253
Stewart ilondanga <stilondanga@gmail.com>
2019-08-13 16:15:31 +0300

    add js/scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,14 @@
+/*$(function(){
+    $(".checkme").on({
+        mouseover:function(){
+            $(this).css({
+                left:(Math.random()*200)+"px",
+                top:(Math.random()*200)+"px",
+            });
+        }
+    });
+});
+
 var navigate = (function() {
 	$('.dd').toggle();
 	$('.dd_btn').click(function() {
